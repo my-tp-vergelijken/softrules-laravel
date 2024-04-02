5eb2f138e3d33f2f408513b4b7963f00d630192f
Sander Muller <sander@mulver.nl>
2024-04-02 14:55:06 +0200

    Update SoftRulesServiceProvider.php

--- src/SoftRulesServiceProvider.php
+++ src/SoftRulesServiceProvider.php
@@ -39,7 +39,7 @@ final class SoftRulesServiceProvider extends PackageServiceProvider
         parent::boot();
 
         $this->publishes([
-            $this->package->basePath('/../../vendor/softrules/php/public/js') => public_path("{$this->package->shortName()}/js"),
+            $this->package->basePath('/../../php/public/js') => public_path("{$this->package->shortName()}/js"),
         ], "{$this->package->shortName()}-assets");
     }
 
